0faf2ef697dad83fe95d796cfd3ba0e91a78a65c
AlexShNT <my_mail_al@mail.ru>
2023-04-28 12:47:34 +0500

    3 sem start

---
+++ 3 seminar/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+bool Poly(int num)
+{
+    int tmp = num;
+    int res = tmp % 10 * 10;
+
+    Console.WriteLine(tmp, res);
+    
+    tmp /=  10;
+    res += tmp % 10;
+    Console.WriteLine(tmp, res);
+
+    tmp /= 100;
+    Console.WriteLine(tmp, res);
+
+}
+
+Console.Write("Input num");
+int num = Console.Convert.ToInt32(Console.ReadLine());
+
+Poly(num);
